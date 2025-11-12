218ab8482779df1f434ec4fc109591d522c3588a
Mehak-Akram <akramabbasi019@gmail.com>
2025-11-12 20:02:10 +0500

    updated

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="UAARN + AI Career Mentor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://uaarn-v11.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
